cc697f660a92405127ad2bb63e2c287221ab79ef
Na7aly <tomac.natalia95@gmail.com>
2024-01-26 00:23:00 +0200

    task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,27 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const startButton = document.querySelector('[data-start]');
+    const stopButton = document.querySelector('[data-stop]');
+    const body = document.body;
+  
+    let colorChangeInterval;
+  
+    function getRandomHexColor() {
+      return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, '0')}`;
+    }
+  
+    function changeBackgroundColor() {
+      body.style.backgroundColor = getRandomHexColor();
+    }
+  
 
+    startButton.addEventListener('click', function () {
+      startButton.disabled = true;
+      colorChangeInterval = setInterval(changeBackgroundColor, 1000);
+    });
+
+    stopButton.addEventListener('click', function () {
+      clearInterval(colorChangeInterval);
+      startButton.disabled = false;
+    });
+  });
+  
